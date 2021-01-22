36607bf48ddc457aa7343a04f4f65f23dc579af7
Martin <martindinev2003@gmail.com>
2021-01-22 16:05:06 +0200

    Add ne funcitons:

--- env/main.py
+++ env/main.py
@@ -28,7 +28,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    #messages = db.relationship('message', backref='user')
+    subjects = db.relationship('Subject',
+            foreign_keys='Subject.user_id',
+            backref='User')
+    posts = db.relationship('Post',
+            foreign_keys='Post.user_id',
+            backref='User')
+    messages = db.relationship('Message',
+            foreign_keys='Message.user_id',
+            backref='User'
+            )
 
     def __init__(self, **kwargs):
         if 'password' in kwargs:
@@ -49,7 +58,6 @@ class User(db.Model):
     def find_by_token(token):
         if not token:
             return None
-
         try:
             s = Serializer(app.secret_key)
             payload = s.loads(token)
@@ -57,22 +65,31 @@ class User(db.Model):
         except SignatureExpired:
             return None
 
-#class Subject:
-#    id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String(80), unique=True, nullable=False)
-#    description = db.Column(db.String(500), unique=True)
-#    posts = db.relationship('Post', backref='subject')
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(500), unique=True)
+    posts = db.relationship('Post',
+            foreign_keys='Post.subject_id',
+            backref='Subject')
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
-#class Post:
-#    id = db.Column(db.Integer, primary_key=True)
-#    messages = db.relationship('message', backref='post')
-#    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    messages = db.relationship('Message',
+            foreign_keys='Message.post_id',
+            backref='Post'
+            )
+    subject_id = db.Column(db.Integer, db.ForeignKey(Subject.id))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
-#class Message:
-#    id = db.Column(db.Integer, primary_key=True)
-#    content = db.Column(db.String(1000), nullable=False)
-#    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-#    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(1000), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(Post.id))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
 db.create_all()
@@ -148,6 +165,93 @@ def register():
         flash('Error: Username is already used!')
         return redirect(request.url)
 
+"""
+TODO:
+@app.route('/my_subjects'), methods=['GET', 'POST']
+
+@app.route('/my_posts'), methods=['GET', 'POST']
+"""
+
+@app.route('/add_subject', methods=['GET', 'POST'])
+@require_login
+def add_subject():
+    if request.method == 'GET':
+        return render_template('add_subject.html')
+    else:
+        name = request.form.get("name")
+        token = request.cookies.get('token')
+        current_user = User.find_by_token(token)
+        description = request.form.get('description')
+        user_id = current_user.id
+
+    try:
+        subject = Subject(
+                name=name,
+                description=description,
+                user_id = user_id
+                )
+        db.session.add(subject)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        flash('Error: {}'.format(e))
+        return redirect(request.url)
+
+@app.route('/edit_subject/<subject_id>', methods=['GET', 'POST'])
+@require_login
+def edit_subject(subject_id):
+    if request.method == 'GET':
+        return render_template('edit_subject.html')
+    else:
+        name = request.form.get("name")
+        description = request.form.get('description')
+
+    try:
+        subject = Subject.query.get(subject_id)
+        subject.name = name
+        subject.description = description
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        flash('Error: {}'.format(e))
+        return redirect(request.url)
+
+@app.route('/delete_subject/<subject_id>', methods=['POST'])
+@require_login
+def delete_subject(subject_id):
+    try:
+        subject = Subject.query.get(subject_id)
+        db.session.delete(subject)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        flash('Error: {}'.format(e))
+        return redirect(request.url)
+
+"""@app.route('/my_posts/add_post/<subject_id>', methods=['GET', 'POST'])
+@require_login
+def add_post(subject_id):
+    if request.method == 'GET':
+        return render_template('add_post.html')
+    else:
+        name = request.form.get("name")
+        subject_id = Subject.query.filter_by(id=subject_id)
+        token = request.cookies.get('token')
+        current_user = User.find_by_token(token)
+        user_id = current_user.id
+
+    try:
+        subject = Subject(
+                name = name,
+                subject_id = subject_id,
+                user_id = user_id
+                )
+        db.session.add(subject)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        flash('Error: {}'.format(e))
+        return redirect(request.url)"""
 
 if __name__ == "__main__":
 	app.run()
