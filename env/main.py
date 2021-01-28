d5e25949b4f802be24a1d7a2a2e212b59c06f14a
nicolaMAN <nicola.manoilov@gmail.com>
2021-01-29 00:18:03 +0200

    update

--- env/main.py
+++ env/main.py
@@ -1,8 +1,9 @@
-from sqlalchemy import asc
+from enum import unique
 import json
 import hashlib
 from functools import wraps
-from flask import request, Flask, flash, redirect, jsonify
+from operator import pos
+from flask import request, Flask, flash, redirect, jsonify, url_for
 from flask import render_template
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
@@ -19,10 +20,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/dev.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.secret_key = "wakbcawiluchawoedhaewu2342bwa"
 
+#================================================================#
+# database
 db = SQLAlchemy(app)
-
-def hash_password(password):
-    return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,10 +34,6 @@ class User(db.Model):
     posts = db.relationship('Post',
             foreign_keys='Post.user_id',
             backref='User')
-    """messages = db.relationship('Message',
-            foreign_keys='Message.user_id',
-            backref='User'
-            )"""
 
     def __init__(self, **kwargs):
         if 'password' in kwargs:
@@ -69,36 +65,24 @@ class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(500), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     posts = db.relationship('Post',
             foreign_keys='Post.subject_id',
             backref='Subject')
-    """messages = db.relationship('Message',
-            foreign_keys='Message.subject_id',
-            backref='Subject'
-            )"""
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True, nullable=False)
-    """messages = db.relationship('Message',
-            foreign_keys='Message.post_id',
-            backref='Post'
-            )"""
-    subject_id = db.Column(db.Integer, db.ForeignKey(Subject.id))
+    name = db.Column(db.String(1000), nullable=False)
+    subject_id = db.Column(db.Integer,db.ForeignKey(Subject.id))
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-"""class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(1000), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(Post.id))
-    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
-    subject_id = db.Column(db.Integer, db.ForeignKey(Subject.id))
-    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-"""
+    sub = db.Column(db.String)
 
 db.create_all()
+
+#---------------------------------------------------------------#
+# functions
 def verify_token(token):
     s = Serializer(app.secret_key)
     try:
@@ -129,13 +113,18 @@ def stop_logged_users(func):
         return func(*args, **kwargs)
     return wrapper
 
-@app.route("/")
-def hello():
-    token = request.cookies.get('token')
-    current_user = User.find_by_token(token)
-    all = Subject.query.all()
-    subjects = list(all)
-    return render_template("base.html", current_user=current_user, subjects=subjects)
+def hash_password(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+def post_return():
+	return Post.query.all()
+
+def subject_return():
+    page = request.args.get('page', 1, type=int)
+    return Subject.query.order_by( Subject.timestamp.asc() ).paginate(page=page, per_page=3)
+
+#...............................................................#
+#authification
 
 @app.route('/login', methods=['GET', 'POST'])
 @stop_logged_users
@@ -173,9 +162,24 @@ def register():
         flash('Error: Username is already used!')
         return redirect(request.url)
 
-@app.route('/my_subjects', methods=['GET'])
+#---------------------------------------------------------------#
+# base 
+
+@app.route("/")
+@app.route("/base", methods = ['GET','POST'])
+def index():
+    token = request.cookies.get('token')
+    current_user = User.find_by_token(token)
+    all = Subject.query.all()
+    subjects = list(all)
+    return render_template("base.html", current_user=current_user, subjects=subjects)
+
+#--------------------------------------------------------------#
+# subject
+
+@app.route('/my_subjects', methods = ['GET'])
 @require_login
-def subjects():
+def my_subjects():
     token = request.cookies.get('token')
     current_user = User.find_by_token(token)
     all = Subject.query.filter_by(user_id = current_user.id)
@@ -184,7 +188,7 @@ def subjects():
 
 @app.route('/my_subjects/<subject_id>', methods=['GET'])
 @require_login
-def subject(subject_id):
+def my_posts(subject_id):
     subject = Subject.query.get(subject_id)
     token = request.cookies.get('token')
     current_user = User.find_by_token(token)
@@ -192,16 +196,16 @@ def subject(subject_id):
     posts = list(all)
     return render_template('subject.html', subject_id=subject_id, subject=subject, posts=posts, current_user=current_user)
 
-@app.route('/<subject_id>', methods=['GET'])
-def subject2(subject_id):
+@app.route('/<subject_id>', methods = ['GET'])
+def subject_2(subject_id):
     subject = Subject.query.get(subject_id)
     token = request.cookies.get('token')
     current_user = User.find_by_token(token)
-    all = Post.query.filter_by(subject_id = subject_id).order_by(asc(Post.timestamp)).all()
+    all = Post.query.filter_by(subject_id = subject_id).order_by(Post.timestamp.asc()).all()
     posts = list(all)
     return render_template('subject.html', subject_id=subject_id, subject=subject, current_user=current_user, posts=posts)
 
-@app.route('/my_subjects/add_subject', methods=['GET', 'POST'])
+@app.route('/my_subjects/add_subject', methods = ['GET', 'POST'])
 @require_login
 def add_subject():
     token = request.cookies.get('token')
@@ -209,7 +213,7 @@ def add_subject():
     if request.method == 'GET':
         return render_template('add_subject.html', current_user=current_user)
     else:
-        name = request.form.get('name')
+        name= request.form.get('name')
         token = request.cookies.get('token')
         current_user = User.find_by_token(token)
         description = request.form.get('message')
@@ -229,7 +233,7 @@ def add_subject():
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-@app.route('/my_subjects/<subject_id>/edit_subject', methods=['GET', 'POST'])
+@app.route('/my_subjects/<subject_id>/edit_subject', methods = ['GET', 'POST'])
 @require_login
 def edit_subject(subject_id):
     if request.method == 'GET':
@@ -251,7 +255,7 @@ def edit_subject(subject_id):
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-@app.route('/my_subjects/<subject_id>/delete_subject', methods=['GET'])
+@app.route('/my_subjects/<subject_id>/delete_subject', methods = ['GET'])
 @require_login
 def delete_subject(subject_id):
     try:
@@ -259,9 +263,6 @@ def delete_subject(subject_id):
         posts = Post.query.filter_by(subject_id=subject_id)
         for post in posts:
             db.session.delete(post)
-        """messages = Message.query.get(subject_id=subject_id)
-        for message in messages:
-            db.session.delete(message)"""
         db.session.delete(subject)
         db.session.commit()
         return redirect('/my_subjects')
@@ -269,9 +270,12 @@ def delete_subject(subject_id):
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-@app.route('/my_subjects/<subject_id>/my_posts', methods=['GET'])
+#----------------------------------------------------------------#
+#posts
+
+@app.route('/my_subjects/<subject_id>/my_posts', methods = ['GET'])
 @require_login
-def posts(subject_id):
+def my_posts(subject_id):
     subject = Subject.query.get(subject_id)
     token = request.cookies.get('token')
     current_user = User.find_by_token(token)
@@ -279,7 +283,7 @@ def posts(subject_id):
     posts = list(all)
     return render_template('my_posts.html', subject_id=subject_id, subject=subject, posts=posts, current_user=current_user)
 
-@app.route('/my_subjects/<subject_id>/my_posts/add_post', methods=['GET', 'POST'])
+@app.route('/my_subjects/<subject_id>/my_posts/add_post', methods = ['GET', 'POST'])
 @require_login
 def add_post(subject_id):
     if request.method == 'GET':
@@ -301,12 +305,12 @@ def add_post(subject_id):
                 )
         db.session.add(post)
         db.session.commit()
-        return redirect('/my_subjects/<subject_id>/my_posts')
+        return redirect('/my_subjects/<subject_id>')
     except Exception as e:
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-@app.route('/<subject_id>/add_post', methods=['GET', 'POST'])
+@app.route('/<subject_id>/add_post', methods = ['GET', 'POST'])
 def add_post_2(subject_id):
     if request.method == 'GET':
         return render_template('add_post.html', subject_id=subject_id)
@@ -326,7 +330,7 @@ def add_post_2(subject_id):
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-@app.route('/my_subjects/<subject_id>/my_posts/<post_id>', methods=['GET'])
+@app.route('/my_subjects/<subject_id>/my_posts/<post_id>', methods = ['GET'])
 @require_login
 def post(subject_id, post_id):
     token = request.cookies.get('token')
@@ -336,7 +340,7 @@ def post(subject_id, post_id):
     return render_template("post.html", subject_id=subject_id, post_id=post_id,
     subject=subject, post=post, current_user=current_user)
 
-@app.route('/my_subjects/<subject_id>/my_posts/<post_id>/edit_post', methods=['GET', 'POST'])
+@app.route('/my_subjects/<subject_id>/my_posts/<post_id>/edit_post', methods = ['GET', 'POST'])
 @require_login
 def edit_post(subject_id, post_id):
     if request.method == 'GET':
@@ -358,7 +362,7 @@ def edit_post(subject_id, post_id):
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-@app.route('/my_subjects/<subject_id>/my_posts/<post_id>/delete_post', methods=['GET'])
+@app.route('/my_subjects/<subject_id>/my_posts/<post_id>/delete_post', methods = ['GET'])
 @require_login
 def delete_post(post_id):
     try:
@@ -373,13 +377,6 @@ def delete_post(post_id):
         flash('Error: {}'.format(e))
         return redirect(request.url)
 
-#TODO:
-@app.route('/<subject_id>/<post_id>', methods=['GET', 'POST'])
-def messaging(subject_id, post_id):
-    if request.method == 'GET':
-        return render_template('messages.html', subject_id=subject_id, post_id=post_id)
-    else:
-        pass
 
 if __name__ == "__main__":
 	app.run()
